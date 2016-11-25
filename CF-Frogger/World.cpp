e534e0d2c4063bca282b6d0f6e769a178e0c1e8d
DESKTOP-UFJ4C6K\bunny <bunnycat1007@gmail.com>
2016-11-25 13:34:02 -0400

    Finished life icons

--- CF-Frogger/World.cpp
+++ CF-Frogger/World.cpp
@@ -223,12 +223,21 @@ namespace GEX
 		_player->setPosition(_spawnPosition);
 		_sceneLayers[Ground]->attatchChild(std::move(Frog));
 
-		/*std::unique_ptr<SpriteNode> _background2(new SpriteNode(texture2, textureRect));
-		_background2->setPosition(_worldBounds.left, _worldBounds.top);
-		_sceneLayers[Air]->attatchChild(std::move(_background2));
+		// Frog lives
+		sf::Texture& texture2 = TextureHolder::getInstance().get(TextureID::FrogLives);
+		sf::IntRect textureRect2(395,100,39,40);
 
+		std::unique_ptr<SpriteNode> _frogLife1(new SpriteNode(texture2, textureRect2));
+		_frogLife1->setPosition(_worldView.getSize().x / 5.f, _worldBounds.height - 50);
+		_sceneLayers[Background]->attatchChild(std::move(_frogLife1));
 
-		addEnemies();*/
+		std::unique_ptr<SpriteNode> _frogLife2(new SpriteNode(texture2, textureRect2));
+		_frogLife2->setPosition(_worldView.getSize().x / 8.f, _worldBounds.height - 50);
+		_sceneLayers[Background]->attatchChild(std::move(_frogLife2));
+
+		std::unique_ptr<SpriteNode> _frogLife3(new SpriteNode(texture2, textureRect2));
+		_frogLife3->setPosition(_worldView.getSize().x / 20.f, _worldBounds.height - 50);
+		_sceneLayers[Background]->attatchChild(std::move(_frogLife3));
 	}
 
 	void World::handleCollisions()
