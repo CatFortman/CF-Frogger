257fc9fc3bd4abbf3499367ed231dd22de82f3e8
DESKTOP-UFJ4C6K\bunny <bunnycat1007@gmail.com>
2016-11-25 12:26:42 -0400

    Initial commit for spawning of frogger

--- CF-Frogger/DataTables.cpp
+++ CF-Frogger/DataTables.cpp
@@ -33,7 +33,7 @@ namespace GEX
 
 		data[Frog::Type::Frogger].hitPoints = 100;
 		data[Frog::Type::Frogger].texture = TextureID::Frog;
-		data[Frog::Type::Frogger].textureRect = sf::IntRect(0, 0, 48, 64);
+		data[Frog::Type::Frogger].textureRect = sf::IntRect(395, 100,39,40);
 
 		return data;
 	}
